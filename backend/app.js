41711c8179ecea2c3ec447c81f7f77f20a3a4094
AdityaKumar41 <119885098+AdityaKumar41@users.noreply.github.com>
2024-05-26 22:50:31 +0530

    change backend url

--- backend/app.js
+++ backend/app.js
@@ -1,75 +1,67 @@
 const cookieSession = require("cookie-session");
-const IPFS=require('ipfs-http-client')
+const IPFS = require("ipfs-http-client");
 const express = require("express");
-const auth=require('./middlewares/auth')
+const auth = require("./middlewares/auth");
 const cors = require("cors");
 
 const passportSetup = require("./passport");
 const passport = require("passport");
 const authRoute = require("./routes/auth");
 const app = express();
-const fileUpload=require('./routes/fileupload')
-const adminRoute=require('./routes/admin')
-require('dotenv').config()
+const fileUpload = require("./routes/fileupload");
+const adminRoute = require("./routes/admin");
+require("dotenv").config();
 
-
-const errorMiddleware=require('./middlewares/error')
+const errorMiddleware = require("./middlewares/error");
 if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({ path: __dirname+"/config/config.env"});
+  require("dotenv").config({ path: __dirname + "/config/config.env" });
 }
 
-const PORT=process.env.PORT||7000
+const PORT = process.env.PORT || 7000;
 //Database connectivity
-const connectDataBase=require('./config/databBase');
+const connectDataBase = require("./config/databBase");
 
 connectDataBase();
 
-
-
 app.use(
   cors({
-    origin: "https://blockation-1.onrender.com",
+    origin: "https://seal-app-35w5i.ondigitalocean.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
 );
 
-app.use(express.json())
-app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //cookie session for passport
 app.use(
-  cookieSession({ name: "session", keys: ["profile","email"], maxAge: 24 * 60 * 60 * 100 })
+  cookieSession({
+    name: "session",
+    keys: ["profile", "email"],
+    maxAge: 24 * 60 * 60 * 100,
+  })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use("/auth", authRoute);
-app.use('/file',fileUpload);
-app.use('/admin',adminRoute)
-app.get('/',(req,res)=>{
-  if(req.user){
-    res.status(200).json(req.user)
-  }
-  else{
+app.use("/file", fileUpload);
+app.use("/admin", adminRoute);
+app.get("/", (req, res) => {
+  if (req.user) {
+    res.status(200).json(req.user);
+  } else {
     res.status(200).json({
-      success:true,
-      message:'User not got'
-    })
+      success: true,
+      message: "User not got",
+    });
   }
-})
-
-
-
-
-
-
-
+});
 
 //error Middleware
-app.use(errorMiddleware)
-
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at the port ${PORT}`);
