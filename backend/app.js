e85f9c8da92e2a066a933d76e399d16fbdf55638
AdityaKumar41 <119885098+AdityaKumar41@users.noreply.github.com>
2024-05-30 02:18:19 +0530

    add mongoess session

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,29 @@
 const cookieSession = require("cookie-session");
-const IPFS=require('ipfs-http-client')
+const IPFS = require("ipfs-http-client");
 const express = require("express");
-const auth=require('./middlewares/auth')
+const auth = require("./middlewares/auth");
 const cors = require("cors");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 const passportSetup = require("./passport");
 const passport = require("passport");
 const authRoute = require("./routes/auth");
 const app = express();
-const fileUpload=require('./routes/fileupload')
-const adminRoute=require('./routes/admin')
-require('dotenv').config()
+const fileUpload = require("./routes/fileupload");
+const adminRoute = require("./routes/admin");
+require("dotenv").config();
 
-
-const errorMiddleware=require('./middlewares/error')
+const errorMiddleware = require("./middlewares/error");
 if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({ path: __dirname+"/config/config.env"});
+  require("dotenv").config({ path: __dirname + "/config/config.env" });
 }
 
-const PORT=process.env.PORT||7000
+const PORT = process.env.PORT || 7000;
 //Database connectivity
-const connectDataBase=require('./config/databBase');
+const connectDataBase = require("./config/databBase");
 
 connectDataBase();
-
-
 
 app.use(
   cors({
@@ -34,42 +33,41 @@ app.use(
   })
 );
 
-app.use(express.json())
-app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //cookie session for passport
 app.use(
-  cookieSession({ name: "session", keys: ["profile","email"], maxAge: 24 * 60 * 60 * 100 })
+  session({
+    secret: "secret", // Replace with your own secret
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.DB_URI,
+      ttl: 24 * 60 * 60, // 1 day
+    }),
+  })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use("/auth", authRoute);
-app.use('/file',fileUpload);
-app.use('/admin',adminRoute)
-app.get('/',(req,res)=>{
-  if(req.user){
-    res.status(200).json(req.user)
-  }
-  else{
+app.use("/file", fileUpload);
+app.use("/admin", adminRoute);
+app.get("/", (req, res) => {
+  if (req.user) {
+    res.status(200).json(req.user);
+  } else {
     res.status(200).json({
-      success:true,
-      message:'User not got'
-    })
+      success: true,
+      message: "User not got",
+    });
   }
-})
-
-
-
-
-
-
-
+});
 
 //error Middleware
-app.use(errorMiddleware)
-
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at the port ${PORT}`);
--- backend/config/databBase.js
+++ backend/config/databBase.js
@@ -1,14 +1,16 @@
-const mongoose=require('mongoose');
-require('dotenv').config()
-const connectDataBase=()=>{
-    mongoose.set('strictQuery',true)
-    mongoose.connect(process.env.DB_URI)
-  
-    .then((data)=>{
-      
-    console.log(`MongoDB is connected successsfully ${data.connection.host}`)
+const mongoose = require("mongoose");
+require("dotenv").config();
+const connectDataBase = () => {
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
-  
-}
 
-module.exports=connectDataBase;
+    .then((data) => {
+      console.log(`MongoDB is connected successsfully ${data.connection.host}`);
+    });
+};
+
+module.exports = connectDataBase;
